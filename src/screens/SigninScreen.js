46055ff74412378c13fe52039752d4c5a8d12114
Toshiyana <yanagimoto-toshiki@dmm.com>
2022-11-12 18:47:11 +0900

    Clearing error messages

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,15 +1,18 @@
 import React, { useContext } from "react";
 import { View, StyleSheet, Text } from "react-native";
-import { Context as AuthContext } from "../context/AuthContext";
+import { NavigationEvents } from "react-navigation";
 import AuthForm from "../components/AuthForm";
 import NavLink from "../components/NavLink";
 import { Context } from "../context/AuthContext";
 
 const SigninScreen = () => {
-  const { state, signin } = useContext(Context);
+  const { state, signin, clearErrorMessage } = useContext(Context);
 
   return (
     <View style={styles.container}>
+      <NavigationEvents
+        onWillFocus={clearErrorMessage} // viewWillDisappear() in swift
+      />
       <AuthForm
         headerText="Sign In to Your Account"
         errorMessage={state.errorMessage}
