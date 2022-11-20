28d65d310026d213a8712655877d54b5d3732573
Toshiyana <yanagimoto-toshiki@dmm.com>
2022-11-20 20:54:11 +0900

    Reading a locations

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,8 +1,13 @@
+import "../_mockLocation";
 import React, { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
 import { SafeAreaView } from "react-navigation";
-import { requestForegroundPermissionsAsync } from "expo-location";
+import {
+  requestForegroundPermissionsAsync,
+  watchPositionAsync,
+  Accuracy,
+} from "expo-location";
 import Map from "../components/Map";
 
 const TrackCreateScreen = () => {
@@ -14,6 +19,16 @@ const TrackCreateScreen = () => {
       if (!granted) {
         throw new Error("Location permission not granted");
       }
+      await watchPositionAsync(
+        {
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10,
+        },
+        (location) => {
+          console.log(location);
+        }
+      );
     } catch (e) {
       setErr(e);
     }
