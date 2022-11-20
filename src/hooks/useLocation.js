7a229281954a1e2c08b0d9a1a8d8c32491c3ced9
Toshiyana <yanagimoto-toshiki@dmm.com>
2022-11-21 03:30:29 +0900

    Cleaning up after ourselves

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -37,6 +37,12 @@ export default (shouldTrack, callback) => {
       subscriber.remove();
       setSubscriber(null);
     }
+
+    return () => {
+      if (subscriber) {
+        subscriber.remove();
+      }
+    };
   }, [shouldTrack, callback]);
 
   return [err];
