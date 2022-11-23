1aa41f3e38820182ade11a9ee067c8a98ffe4cfc
Toshiyana <yanagimoto-toshiki@dmm.com>
2022-11-23 18:01:36 +0900

    What manages tracks

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -11,7 +11,7 @@ const TrackForm = () => {
     stopRecording,
   } = useContext(LocationContext);
 
-  console.log(locations.length);
+  // console.log(locations.length);
 
   return (
     <>
@@ -22,11 +22,18 @@ const TrackForm = () => {
           placeholder="Enter name"
         />
       </Spacer>
-      {recording ? (
-        <Button title="Stop" onPress={stopRecording} />
-      ) : (
-        <Button title="Start Recording" onPress={startRecording} />
-      )}
+      <Spacer>
+        {recording ? (
+          <Button title="Stop" onPress={stopRecording} />
+        ) : (
+          <Button title="Start Recording" onPress={startRecording} />
+        )}
+      </Spacer>
+      <Spacer>
+        {!recording && locations.length ? (
+          <Button title="Save Recording" />
+        ) : null}
+      </Spacer>
     </>
   );
 };
