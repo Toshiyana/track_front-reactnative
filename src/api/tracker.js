aee31b09005ce8d66470bc33eadd6d67c2df9565
Toshiyana <yanagimoto-toshiki@dmm.com>
2022-11-23 18:52:24 +0900

    Form reset and navigation

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,21 @@
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default axios.create({
-  baseURL: "https://df4e-220-146-163-149.jp.ngrok.io",
+const instance = axios.create({
+  baseURL: "https://cdfa-220-146-163-149.jp.ngrok.io",
 });
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
